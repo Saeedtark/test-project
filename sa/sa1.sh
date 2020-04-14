37daf8b8e87753bbf05e15ac3a89522d1af9bab3
saeedagha <saeedking.0963@gmail.com>
2020-04-14 22:32:24 +0300

    first

---
+++ sa/sa1.sh
@@ -0,0 +1,25 @@
+a=10
+b=10
+if [ $a == $b ]
+then
+echo "a is equal to b"
+fi
+
+if [ $a != $b ]
+then
+echo "a is not equal to b"
+fi
+
+
+a=Apple
+b=oranges
+if [ $Apple == $Apple ]
+then
+echo "Apple is equal to Apple"
+fi
+if [ $Apple == $oranges ]
+then
+echo "Apple is not equal to oranges"
+fi
+
+
---
+++ saeed1.sh
@@ -0,0 +1,10 @@
+a=Apple
+b=oranges
+if [ $Apple == $Apple ]
+then
+echo "Apple is equal to Apple"
+fi
+if [ $Apple != $oranges ]
+then
+echo "Apple is not equal to oranges"
+fi
